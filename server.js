167aa550abe0645229cb2290118871819c479224
Gmtn09093232 <157684016+Gmtn09093232@users.noreply.github.com>
2026-06-11 15:44:01 +0300

    Serve gmtn.html for the root route
    Added a new route to serve gmtn.html for the root path.

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'gmtn.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
